d560992ca496f2e959a124598d3f84b960afdc8e
omveersingh409 <omveersinghshekhawat20@gmail.com>
2026-05-09 19:14:50 +0530

    Allow all Vercel origins to prevent CORS errors on preview URLs

--- server/index.js
+++ server/index.js
@@ -31,11 +31,14 @@ app.use(cors({
     origin: function(origin, callback){
         // allow requests with no origin like mobile apps or curl requests
         if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
+        
+        if(allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')){
+            return callback(null, true);
+        } else {
+            console.error("❌ CORS BLOCKED ORIGIN:", origin);
             const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
             return callback(new Error(msg), false);
         }
-        return callback(null, true);
     },
     credentials: true
 }));
